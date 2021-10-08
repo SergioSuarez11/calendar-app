099a284c646f5f7bf15d7b63f50507bff8e2bb6c
Chaparro27 <48492306+Chaparro27@users.noreply.github.com>
2021-10-07 20:51:13 -0500

    Rutas

---
+++ lib/src/services/Connection_service.dart
@@ -0,0 +1,10 @@
+import 'package:calendar_app/src/services/Connection_service.dart';
+
+class ConnectionService{
+
+    TypeRequestModel _typeRequestModel;
+  List<TypeRequestModel> _listTypeRequest = [];
+  List<RequestsModel> _listRequestsModel  = [];
+
+  
+}
