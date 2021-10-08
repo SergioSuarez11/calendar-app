835640be70f24753bb75d6bd6c6e25c7f5795c3a
Pablo Estrella <almendaris123@gmail.com>
2021-10-08 16:02:50 -0500

    Se agregó el api de calendario v1

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,9 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
 import 'package:calendar_app/src/pages/home_page.dart';
 import 'package:calendar_app/src/pages/register.dart';
 import 'package:calendar_app/src/provider/google_sign_in.dart';
-import 'package:flutter/material.dart';
 import 'package:calendar_app/src/pages/login.dart';
-import 'package:provider/provider.dart';
+import 'package:calendar_app/src/pages/Home.dart';
 
 class CalendarApp extends StatelessWidget {
   @override
@@ -11,12 +13,12 @@ class CalendarApp extends StatelessWidget {
         create: (context) => GoogleSignInProvider(),
         child: MaterialApp(
           debugShowCheckedModeBanner: false,
-          initialRoute: '/',
+          initialRoute: 'calendar',
           routes: {
             '/': (context) => LoginPage(),
             'register': (context) => RegisterPage(),
             'home_page':(context)=> HomePage(),
-
+            'calendar' : ( _ ) => Home()
           },
 
           //LoginPage(),
